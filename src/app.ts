31f6aff62048e1a8a170cc668c356ea5810122cb
Disyer <daniel@tohka.us>
2024-01-26 12:06:37 +0200

    jwt: Add initial JWT configuration

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,20 @@
 import express, { Request, Response } from "express";
 import mongoose, { Document } from "mongoose";
 import { body, validationResult } from "express-validator";
+import { EachMessagePayload, Kafka, KafkaConfig } from "kafkajs";
+import { encode, decode } from "@msgpack/msgpack";
+import { KeyLike, jwtVerify } from "jose";
+
+interface JwtRotatedPayload {
+  jwt: string;
+}
+
+interface JwtUser {
+  id: string;
+  username: string;
+  email: string;
+  roles: string[];
+}
 
 const app = express();
 const port = 5001;
@@ -9,10 +23,62 @@ let connectionString = process.env.MONGO_CONNECTION_STRING;
 
 if (!connectionString) {
   connectionString =
-    "mongodb://user:pass@localhost:27017/azoradb?authSource=admin";
+    "mongodb://user:pass@localhost:27017/videos?authSource=admin";
 }
 
 mongoose.connect(connectionString, {});
+
+const kafkaConfig: KafkaConfig = {
+  brokers: [process.env.KAFKA_URI],
+  sasl: {
+    mechanism: "scram-sha-256",
+    username: process.env.KAFKA_USERNAME,
+    password: process.env.KAFKA_PASSWORD,
+  },
+};
+const kafka = new Kafka(kafkaConfig);
+
+let jwtSecret: Uint8Array | null = null;
+
+const jwtConsumer = kafka.consumer({ groupId: null });
+
+jwtConsumer.connect().then(() => {
+  jwtConsumer
+    .subscribe({ topic: "jwt-rotated", fromBeginning: true })
+    .then(() => {
+      jwtConsumer.run({
+        eachMessage: async ({
+          topic,
+          partition,
+          message,
+        }: EachMessagePayload) => {
+          const data = decode(message.value) as JwtRotatedPayload;
+          console.log("data", data);
+          var enc = new TextEncoder();
+          jwtSecret = enc.encode(data.jwt);
+        },
+      });
+    });
+});
+
+const getBearerToken = (req) => {
+  if (
+    !req.headers.authorization ||
+    req.headers.authorization.split(" ")[0] !== "Bearer"
+  ) {
+    throw new Error("Invalid Authorization header");
+  }
+
+  return req.headers.authorization.split(" ")[1];
+};
+
+const decodeJWT = async (req) => {
+  const token = getBearerToken(req);
+  const { payload } = await jwtVerify(token, jwtSecret);
+
+  console.log(payload);
+  return payload as unknown as JwtUser;
+};
 
 const videoSchema = new mongoose.Schema({
   title: { type: String, required: true, minlength: 1, maxlength: 100 },
@@ -40,7 +106,6 @@ app.post(
   [
     body("title").isLength({ min: 1, max: 100 }),
     body("description").isLength({ max: 500 }),
-    body("owner_user_id").isLength({ min: 1 }),
   ],
   async (req: Request, res: Response) => {
     const errors = validationResult(req);
@@ -49,7 +114,15 @@ app.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const video = new VideoModel(req.body);
+    const user = await decodeJWT(req);
+
+    const data = {
+      title: req.body.title,
+      description: req.body.description,
+      owner_user_id: user.id,
+    };
+
+    const video = new VideoModel(data);
 
     try {
       const savedVideo = await video.save();
