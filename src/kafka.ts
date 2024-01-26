fe1b4591e26b7139ca46401c353cd37bf36918f5
Disyer <daniel@tohka.us>
2024-01-26 13:49:47 +0200

    video: Add video deleted payload

--- src/kafka.ts
+++ src/kafka.ts
@@ -1,10 +1,21 @@
-import { EachMessagePayload, Kafka, KafkaConfig } from "kafkajs";
+import {
+  Consumer,
+  ConsumerSubscribeTopics,
+  EachMessagePayload,
+  Kafka,
+  KafkaConfig,
+} from "kafkajs";
 import { decode } from "@msgpack/msgpack";
 import { v4 as uuidv4 } from "uuid";
 import { setJwtSecret } from "./jwt.js";
+import { VideoModel } from "./mongodb/video.js";
 
 interface JwtRotatedPayload {
   jwt: string;
+}
+
+interface UserDeletedPayload {
+  id: string;
 }
 
 const kafkaConfig: KafkaConfig = {
@@ -18,25 +29,51 @@ const kafkaConfig: KafkaConfig = {
 const kafka = new Kafka(kafkaConfig);
 
 const jwtConsumer = kafka.consumer({ groupId: `jwt-group-${uuidv4()}` });
+const userDeletedConsumer = kafka.consumer({
+  groupId: "user-deleted-group",
+});
 
 const onJwtRotated = async (data: JwtRotatedPayload) => {
   console.log("Rotated JWT secret:", data.jwt);
   setJwtSecret(new TextEncoder().encode(data.jwt));
 };
 
-const setupKafka = () => {
-  return jwtConsumer.connect().then(() => {
-    jwtConsumer
-      .subscribe({ topic: "jwt-rotated", fromBeginning: true })
-      .then(() => {
-        jwtConsumer.run({
-          eachMessage: async ({ message }: EachMessagePayload) => {
-            const data = decode(message.value) as JwtRotatedPayload;
-            onJwtRotated(data);
-          },
-        });
+const onUserDeleted = async (data: UserDeletedPayload) => {
+  const { id } = data;
+
+  const result = await VideoModel.deleteMany({ owner_user_id: id });
+  console.log(`User deleted. Removed ${result.deletedCount} videos`);
+};
+
+const setupListener = <T>(
+  consumer: Consumer,
+  topic: ConsumerSubscribeTopics,
+  onEventReceived: (data: T) => void
+) => {
+  return consumer.connect().then(() => {
+    consumer.subscribe(topic).then(() => {
+      consumer.run({
+        eachMessage: async ({ message }: EachMessagePayload) => {
+          const data = decode(message.value) as T;
+          onEventReceived(data);
+        },
       });
+    });
   });
+};
+
+const setupKafka = () => {
+  setupListener<JwtRotatedPayload>(
+    jwtConsumer,
+    { topics: ["jwt-rotated"], fromBeginning: true },
+    onJwtRotated
+  );
+
+  return setupListener<UserDeletedPayload>(
+    userDeletedConsumer,
+    { topics: ["user-deleted"] },
+    onUserDeleted
+  );
 };
 
 export { setupKafka };
